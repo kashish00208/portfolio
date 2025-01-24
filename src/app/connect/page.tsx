aae5990213bdb7b54638e3a895d777abb5068034
kashish00208 <kgupta7827@gmail.com>
2025-01-24 23:45:28 +0530

    added more pages

---
+++ src/app/connect/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      connect
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/tools/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      tool
+    </div>
+  )
+}
+
+export default page
