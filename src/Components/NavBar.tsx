e6c277c469602941bc875845785d3b2d288b1aa9
kashish00208 <kgupta7827@gmail.com>
2025-01-24 14:51:10 +0530

    navbar added

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React from 'react'
-
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
 const NavBar = () => {
   return (
     <div>
-      NavBar
+      <div></div>
     </div>
   )
 }
