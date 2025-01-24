422f2bb266fab4aacb1bb9e7dcfae90519f563b2
kashish00208 <kgupta7827@gmail.com>
2025-01-24 23:35:01 +0530

    profile page

--- src/Components/about/Intro.tsx
+++ src/Components/about/Intro.tsx
@@ -1,12 +1,29 @@
-'use client'
-import React from 'react'
-
+"use client";
+import React from "react";
+import Image from "next/image";
 const about = () => {
   return (
-    <div>
-      Intro
+    <div className="w-1/4 mx-auto bg-white fixed top-28 ml-20 mr-9 h-auto rounded-xl">
+      <div className="my-5 flex justify-center items-center flex-col  p-3">
+        <div className="">
+          <Image
+            src={"/img1.jpg"}
+            width={200}
+            height={200}
+            alt="profile"
+            className="rounded-xl"
+          />
+        </div>
+        <h1 className="text-black text-4xl font-bold font-sans my-5">
+          Kashish Gupta
+        </h1>
+        <p className="text-gray-700 text-sm text-center">
+          A software engineer driven by a desire to solve complex problems and
+          build cutting-edge applications
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
