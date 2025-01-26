ffa7a2b649bad89682eb86a2d64fa283fe4ab155
kashish00208 <kgupta7827@gmail.com>
2025-01-26 22:26:30 +0530

    minor changes

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="mt-10 ml-10 mb-20">
+    <div className="mt-10 mb-20 w-2/4  top-20 left-96 ml-16 fixed h-auto rounded-xl">
       <p className="text-white text-8xl font-semibold">SOFTWARE </p>
       <p className="text-white text-7xl text-opacity-50">DEVELOPER</p>
       <p className="mt-7 max-w-96 text-white text-opacity-45">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Project from "@/app/Project/page"
 import tools from "@/app/Tools/page"
 import Homepage from '@/app/Homepage/page'
 import Connect from '@/app/Connect/page'
+import Intro from '@/components/about/Intro'
 export default function Home() {
   return (
     <>
+      <div>
+      <Intro />
+      </div>
       <div className="w-2/4  top-20 left-96 ml-16 fixed h-auto rounded-xl">
       <Homepage/>
-      <Project/>
       </div>
     </>
   );
