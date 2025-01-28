f072fcbf569e60072fb840ab99dbfe02826102d4
kashish00208 <kgupta7827@gmail.com>
2025-01-28 14:53:38 +0530

    changes

--- src/Components/Links/ProjectLinks.tsx
+++ src/Components/Links/ProjectLinks.tsx
@@ -1,24 +1,36 @@
 import { div, hr } from "motion/react-client";
 import Link from "next/link";
 import Image from "next/image";
-export const projectLinks = ({
-    title,
-    description,
-    href,
-    src,
-}:{
-    title: string;
-    description: string;
-    href : string,
-    src : string
-})=>{
-    return (
-        <Link href={href}>
-            <div>
-                <Image src={src} alt={title}/>
-                {title}
-                {description}
-            </div>
-        </Link>
-    )
-}
+export const ProjectLinks = ({
+  title,
+  description,
+  href,
+  src,
+}: {
+  title: string;
+  description: string;
+  href: string;
+  src: string;
+}) => {
+  return (
+    <Link href={href}>
+      <div className="flex my-10  hover:bg-slate-700 hover:bg-opacity-10 p-6 rounded-md ">
+        <div className="mr-10">
+          <Image
+            src={src}
+            alt={title}
+            width={120}
+            height={100}
+            className="rounded-md"
+          />
+        </div>
+        <div className="flex flex-col justify-center gap-1 ">
+          <p className="text-2xl font-bold text-white ">{title}</p>
+          <p className="text-white text-opacity-50">{description}</p>
+        </div>
+        
+      </div>
+    </Link>
+  );
+};
+export default ProjectLinks;
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import ProjectLinks from "@/components/Links/ProjectLinks";
 const page = () => {
   return (
     <div className="mt-10 mb-20 w-2/4  top-20 left-96 ml-16 fixed h-auto rounded-xl">
-      <p className="text-white text-8xl font-semibold">RECENT</p>
-      <p className="text-white text-7xl text-opacity-50">PROJECTS</p>
+      <div>
+        <p className="text-white text-8xl font-semibold">RECENT</p>
+        <p className="text-white text-7xl text-opacity-50">PROJECTS</p>
+      </div>
+      <div className="projectlinks "></div>
+      <ProjectLinks
+        title="YTExtractor"
+        description="Youtube extractor"
+        href="https://github.com/kashish00208/YTExtractor"
+        src="/img1.jpg"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
