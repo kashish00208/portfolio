ee65bbf90e8b319c7c27547358f6e5dbf04ee463
kashish00208 <kgupta7827@gmail.com>
2025-01-31 01:14:18 +0530

    app.jsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-'use client'
-import Project from "@/app/Project/page"
-import tools from "@/app/Tools/page"
 import Homepage from '@/app/Homepage/page'
-import Connect from '@/app/Connect/page'
-import Intro from '@/components/about/Intro'
 export default function Home() {
   return (
     <>
