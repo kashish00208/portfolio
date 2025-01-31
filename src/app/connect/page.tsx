111170ad077976b9993ba0a2a6c8828c80273745
kashish00208 <kgupta7827@gmail.com>
2025-01-31 15:10:18 +0530

    changes

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+"use client";
+import React from "react";
+import ImpLinks from "@/components/Links/ImpLink";
+import Intro from "@/components/about/Intro";
 
 const page = () => {
   return (
-    <div className="mt-10 mb-20 w-2/4  top-20 left-96 ml-16 fixed h-auto rounded-xl">
-      <p className="text-white text-8xl font-semibold">Tools</p>
-    </div>
-  )
-}
+    <>
+      <div className="m-7 text-center flex flex-col lg:flex-row ">
+        <div className="flex-1 md:ml-96 md:mt-20 md:text-start">
+          <div className="md:ml-10 lg:ml-10">
+            <p className="text-white text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold ">
+            LET'S WORK
+            </p>
+            <p className="text-white text-4xl sm:text-4xl md:text-5xl lg:text-7xl text-opacity-50">
+            TOGETHER
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
