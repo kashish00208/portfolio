25d2ae4e50091e7e50bb243133369c03704b203c
kashish00208 <kgupta7827@gmail.com>
2025-03-16 16:28:43 +0530

    deployement issue fix

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -59,7 +59,7 @@ const page = () => {
         <div className="flex-1 md:ml-96 md:mt-20 md:text-start">
           <div className="md:ml-10 lg:ml-10">
             <p className="text-white text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold">
-            Let&apos;s work
+            Lets work
             </p>
 
             <p className="text-white text-4xl sm:text-4xl md:text-5xl lg:text-7xl text-opacity-50">
