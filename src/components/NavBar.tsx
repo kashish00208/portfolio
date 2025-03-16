0f9c43a0b0489f41d7cf39c2179ce0ca1fb65857
kashish00208 <kgupta7827@gmail.com>
2025-03-16 18:06:33 +0530

    changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,22 +11,22 @@ const NavBar = ({ className }: { className?: string }) => {
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Home" icon="/home.png" href="/Homepage">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/Homepage">Home</HoveredLink>
+            <HoveredLink href="/homepage">home</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Projects" icon="/note.png" href="Project">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/Project">Project</HoveredLink>
+            <HoveredLink href="/project">project</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Tools" icon="/tool.png" href="/Tools">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/Tools">Tools</HoveredLink>
+            <HoveredLink href="/tools">tools</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Connect" icon="/project.png" href="/Connect">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/Connect">Connect</HoveredLink>
+            <HoveredLink href="/connect">connect</HoveredLink>
           </div>
         </MenuItem>
       </Menu>
