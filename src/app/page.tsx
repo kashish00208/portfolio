4906fad1bed857751f8a765fb900112bcef98f51
kashish00208 <kgupta7827@gmail.com>
2025-03-16 22:03:57 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import Homepage from './Homepage/page'
-import Connect from'./connect/page'
-import Project from './project/page'
-import Tools from './tools/page'
 
 export default function Home() {
   return (
     <>
       <Homepage/>
-      <Project/>
-      <Tools/>
-      <Connect/>
+      
     </> 
   );
 }
