328a9d324fc3079e65f1255fa039ef8325528b12
kashish00208 <kgupta7827@gmail.com>
2025-06-19 20:38:51 +0530

    minor change

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -12,10 +12,10 @@ const page = () => {
         <div className="flex-1 md:ml-96 md:mt-20 md:text-start">
           <div className="md:ml-10 lg:ml-10">
             <p className="text-white text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold ">
-              SOFTWARE
+              Full Stack
             </p>
             <p className="text-white text-4xl sm:text-4xl md:text-5xl lg:text-7xl text-opacity-50">
-              DEVELOPER
+              Developer
             </p>
             <p className="mt-7 max-w-96 text-white text-opacity-45 text-lg">
               Driven by a love for building seamless and impactful user
